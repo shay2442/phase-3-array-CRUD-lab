5affa2ba2cdae5bcb91f94763b7fecccd74f4193
shaywacker <shay2442@yahoo.com>
2021-12-06 23:17:47 -0600

    commit tree dfbc7fdd48478f7fd3848488cf11f33899a2be19

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,47 @@
 def create_an_empty_array
-  
-end
+    []
+  end
+   
 
 def create_an_array
-  
+  arr = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array = [1,2,3,4]
+    array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["cool", "neat"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
+    array =['this', 'is', 'arrays!']
+    array.pop()
   
 end
 
 def remove_element_from_start_of_array(array)
+    array =['wow', 'this', 'is', 'arrays!']
+    array.shift()
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    array =['wow', 'this', 'am', 'arrays!']
+    array[2]
 end
 
 def retrieve_first_element_from_array(array)
+    array =['wow', 'this', 'am', 'arrays!']
+    array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+    array =['wow', 'this', 'am', 'arrays!']
+    array[-1]
   
 end
